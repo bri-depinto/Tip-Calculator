6f45630a40a3b933b0e81bb4181fb31198edaed7
bri-depinto <briannadepinto@gmail.com>
2024-01-23 10:19:51 -0500

    javaaaaa

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
-function calculateAndDisplay(){
+function calculateAndDisplay() {
    // get the input values from our html forms
-let bill= parseFloat(document.getElementById(totalBill).value)
-let numPpl= parseInt(document.getElementById(numPpl).value)
-let serviceQuality= document.getElementById('serviceQuality').value
+   let bill = parseFloat(document.getElementById('totalBill').value)
+   let numPpl = parseInt(document.getElementById('numPpl').value)
+   let serviceQuality = document.getElementById('serviceQuality').value
 
    // calculate tip, total bill, and the amount per person using helper function
-let tip= calculateTip(bill,serviceQuality)
-let totalBill= calculateTotralBill(bill,tip)
-let amtPerPerson= calculateAmtPerPerson(totalBill,numPpl)
+   let tip = calculateTip(bill, serviceQuality)
+   let totalBill = calculateTotalBill(bill, tip)
+   let amtPerPerson = calculateAmtPerPerson(totalBill, numPpl)
 
-// display the result in html document
-document.getElementById('tipResult').innerText ='Tip: $' + tip.tofixed(2)
-document.getElementById('totalBillResult').innerText = 'Total Bill: $' +totalBill.toFixed(2)
-document.getElementById('amtPerPersonResult').innerText = 'Amount per person: $' + amtPerPerson.toFixed(2)
+   // display the result in html document
+   document.getElementById('tipResult').innerText = 'Tip: $' + tip.toFixed(2)
+   document.getElementById('totalBillResult').innerText = 'Total Bill: $' + totalBill.toFixed(2)
+   document.getElementById('amtPerPersonResult').innerText = 'Amount per person: $' + amtPerPerson.toFixed(2)
 
 }
+// helper function to calculate tip based on the bill and service quality
 
 
+function calculateTip(bill, quality){
+   if(quality === 'Great'){
+      return bill * 0.2
+  }
+   else if (quality === 'good'){
+      return bill * 0.15
+   }
+   else if(quality === 'poor') {
+      return bill * 0.1
+   }
+   else {
+      return 0
+   }
+}
 
+// helper function to calculte total bill by adding the orginal bill and tip
+function calculateTotalBill(bill,tip){
+   return bill + tip
+}
 
-
-
-
-
-
-
+function calculateAmtPerPerson(totalBill,numPpl){
+   return totalBill/numPpl
+}
 
 
 
